df714b2785a8537a2e8f422f1dee9728b0f77566
xegram-0 <darkbroodlord@gmail.com>
2025-03-27 14:48:29 +0700

    Testing collision and timer

--- stuff.gd
+++ stuff.gd
@@ -1,15 +1,17 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var speed:int = 250
-var acceleration := 2
+@export var speed:int = 300
+#var acceleration := 2
 
 func _ready():
-	#position = Vector2(200,200)
+	position = Vector2(500,200)
 	pass
 	
 func _process(delta):
 	var dir = Input.get_vector("left","right","up","down")
-	print(dir)
-	position += dir * speed * delta * acceleration
+	#print(dir)
+	velocity = dir * speed
+	#position += dir * speed * delta * acceleration
+	move_and_slide()
 	
 		
